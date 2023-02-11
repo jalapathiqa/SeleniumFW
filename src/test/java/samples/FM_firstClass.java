e9e9059390eb800852308df02cbc545a8950f36a
Jalapathi Kalagiri <jalapathiqa@gmail.com>
2023-02-11 00:51:35 -0800

    testing automatic run the tests from Jenkins with ngrok:

--- src/test/java/samples/FM_firstClass.java
+++ src/test/java/samples/FM_firstClass.java
@@ -4,5 +4,6 @@ public class FM_firstClass {
 
 	public static void main(String[] args) {
 		System.out.println("printing a sample class/method for demo purpose");
+		System.out.println("printing 2nd time a sample class/method for demo purpose");
 	}
 }
