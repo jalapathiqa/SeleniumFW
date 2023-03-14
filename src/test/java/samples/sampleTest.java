1bd9af573bd3cd6c449034f93d99e4fb20df162a
Jalapathi Kalagiri <jalapathiqa@gmail.com>
2023-03-13 21:56:52 -0700

    ngrok

--- src/test/java/samples/sampleTest.java
+++ src/test/java/samples/sampleTest.java
@@ -3,7 +3,9 @@ package samples;
 public class sampleTest {
 
 	public static void main(String[] args) {
+		System.out.println("sample test class");	
 		System.out.println("sample test class");
+
 
 	}
 
