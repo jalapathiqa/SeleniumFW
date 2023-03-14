094e146fd7aeb55077d0c7a34cb2801b32f09146
Jalapathi Kalagiri <jalapathiqa@gmail.com>
2023-03-13 21:49:19 -0700

    ngrok update

--- src/test/java/samples/sampleTest.java
+++ src/test/java/samples/sampleTest.java
@@ -3,8 +3,7 @@ package samples;
 public class sampleTest {
 
 	public static void main(String[] args) {
-System.out.println("sample test class");
-System.out.println("sample test class");
+		System.out.println("sample test class");
 
 	}
 
